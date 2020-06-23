86696f389512cc094bbe4285af172fd4cd53ddb2
Aniruddha Shirahatti <aniruddhashirahatti@yahoo.com>
2020-06-23 19:08:10 -0400

    data pipeline initial commit

---
+++ src/main/java/org/anir/beam_pipeline/StarterPipeline.java
@@ -0,0 +1,69 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.anir.beam_pipeline;
+
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * A starter example for writing Beam programs.
+ *
+ * <p>The example takes two strings, converts them to their upper-case
+ * representation and logs them.
+ *
+ * <p>To run this starter example locally using DirectRunner, just
+ * execute it without any additional parameters from your favorite development
+ * environment.
+ *
+ * <p>To run this starter example using managed resource in Google Cloud
+ * Platform, you should specify the following command-line options:
+ *   --project=<YOUR_PROJECT_ID>
+ *   --stagingLocation=<STAGING_LOCATION_IN_CLOUD_STORAGE>
+ *   --runner=DataflowRunner
+ */
+public class StarterPipeline {
+  private static final Logger LOG = LoggerFactory.getLogger(StarterPipeline.class);
+
+  public static void main(String[] args) {
+	  
+	// Create the pipeline.
+	PipelineOptions options = 
+			PipelineOptionsFactory.fromArgs(args).withValidation().create();
+    Pipeline p = Pipeline.create(options);
+    
+    
+    // Reading from an External Source.
+    // Create the PCollection 'lines' by applying a 'Read' transform.
+    p.apply("ReadMyFile", TextIO.read().from("gs://dataflow-cloud-academy-281113/beam_info.txt"))
+     .apply("ExtractWords", ParDo.of(new DoFn<String, String>() {
+         @ProcessElement
+         public void processElement(ProcessContext c) {
+           c.output(c.element().toUpperCase());
+         }
+       }))
+     .apply("WriteMyFile", TextIO.write().to("gs://dataflow-cloud-academy-281113/beam_info_output.txt"));
+
+    p.run().waitUntilFinish();
+  }
+}
