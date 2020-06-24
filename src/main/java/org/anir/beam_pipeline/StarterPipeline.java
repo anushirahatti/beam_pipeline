dfbb47001e162d713336359bee040a5cfe9946aa
Aniruddha Shirahatti <aniruddhashirahatti@yahoo.com>
2020-06-24 10:42:55 -0400

    improving pipeline

--- src/main/java/org/anir/beam_pipeline/StarterPipeline.java
+++ src/main/java/org/anir/beam_pipeline/StarterPipeline.java
@@ -17,8 +17,6 @@
  */
 package org.anir.beam_pipeline;
 
-import javax.annotation.RegEx;
-
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.options.Default;
@@ -26,8 +24,6 @@ import org.apache.beam.sdk.options.Description;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.options.Validation.Required;
-import org.apache.beam.sdk.transforms.DoFn;
-import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.transforms.Regex;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -51,19 +47,39 @@ import org.slf4j.LoggerFactory;
 public class StarterPipeline {
 	
   private static final Logger LOG = LoggerFactory.getLogger(StarterPipeline.class);
+  
+  public interface StarterPipelineOptions extends PipelineOptions {
+
+	    /**
+	     * By default, this example reads from a public dataset containing the text of
+	     * King Lear. Set this option to choose a different input file or glob.
+	     */
+	    @Description("Path of the file to read from")
+	    @Default.String("gs://dataflow-cloud-academy-281113/beam_info.txt")
+	    String getInputFile();
+	    void setInputFile(String value);
+
+	    /**
+	     * Set this required option to specify where to write the output.
+	     */
+	    @Description("Path of the file to write to")
+	    @Required
+	    String getOutput();
+	    void setOutput(String value);
+  }
 
   public static void main(String[] args) {
 	  
 	// Create the pipeline.
-	PipelineOptions options = PipelineOptionsFactory.fromArgs(args).withValidation().create().as(PipelineOptions.class);
+	StarterPipelineOptions options = PipelineOptionsFactory.fromArgs(args).withValidation().as(StarterPipelineOptions.class);
     Pipeline p = Pipeline.create(options);
     
     
     // Reading from an External Source.
     // Create the PCollection 'lines' by applying a 'Read' transform.
-    p.apply("ReadMyFile", TextIO.read().from("gs://dataflow-cloud-academy-281113/beam_info.txt"))
+    p.apply("ReadMyFile", TextIO.read().from(options.getInputFile()))
      .apply("ExtractWords", Regex.split("\\s+"))
-     .apply("WriteMyFile", TextIO.write().to("gs://dataflow-cloud-academy-281113/beam_info_output"));
+     .apply("WriteMyFile", TextIO.write().to(options.getOutput()));
 
     p.run().waitUntilFinish();
   }
