009597394bc01bc80be7b8294e9b4d1dc772c249
Aniruddha Shirahatti <aniruddhashirahatti@yahoo.com>
2020-06-24 09:46:49 -0400

    exploring pipeline

--- src/main/java/org/anir/beam_pipeline/StarterPipeline.java
+++ src/main/java/org/anir/beam_pipeline/StarterPipeline.java
@@ -17,6 +17,8 @@
  */
 package org.anir.beam_pipeline;
 
+import javax.annotation.RegEx;
+
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.options.Default;
@@ -26,6 +28,7 @@ import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.options.Validation.Required;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.transforms.Regex;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -48,40 +51,19 @@ import org.slf4j.LoggerFactory;
 public class StarterPipeline {
 	
   private static final Logger LOG = LoggerFactory.getLogger(StarterPipeline.class);
-  
-  public interface StarterPipelineOptions extends PipelineOptions {
-
-	    @Description("Path of the file to read from")
-	    @Default.String("gs://dataflow-cloud-academy-281113/beam_info.txt")
-	    String getInputFile();
-	    void setInputFile(String value);
-
-	    /**
-	     * Set this required option to specify where to write the output.
-	     */
-	    @Description("Path of the file to write to")
-	    @Required
-	    String getOutput();
-	    void setOutput(String value);
-  }
 
   public static void main(String[] args) {
 	  
 	// Create the pipeline.
-	StarterPipelineOptions options = PipelineOptionsFactory.fromArgs(args).withValidation().create().as(StarterPipelineOptions.class);
+	PipelineOptions options = PipelineOptionsFactory.fromArgs(args).withValidation().create().as(PipelineOptions.class);
     Pipeline p = Pipeline.create(options);
     
     
     // Reading from an External Source.
     // Create the PCollection 'lines' by applying a 'Read' transform.
-    p.apply("ReadMyFile", TextIO.read().from(options.getInputFile()))
-     .apply("ExtractWords", ParDo.of(new DoFn<String, String>() {
-         @ProcessElement
-         public void processElement(ProcessContext c) {
-           c.output(c.element().toUpperCase());
-         }
-       }))
-     .apply("WriteMyFile", TextIO.write().to(options.getOutput()));
+    p.apply("ReadMyFile", TextIO.read().from("gs://dataflow-cloud-academy-281113/beam_info.txt"))
+     .apply("ExtractWords", Regex.split("\\s+"))
+     .apply("WriteMyFile", TextIO.write().to("beam_info_output"));
 
     p.run().waitUntilFinish();
   }
