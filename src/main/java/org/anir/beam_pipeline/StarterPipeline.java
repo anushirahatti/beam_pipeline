c753dc1f33cb2b2c1b5a25a457491d5c667d74b5
Aniruddha Shirahatti <aniruddhashirahatti@yahoo.com>
2020-06-24 11:00:39 -0400

    improving pipeline

--- src/main/java/org/anir/beam_pipeline/StarterPipeline.java
+++ src/main/java/org/anir/beam_pipeline/StarterPipeline.java
@@ -1,20 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.anir.beam_pipeline;
 
 import org.apache.beam.sdk.Pipeline;
@@ -28,22 +12,7 @@ import org.apache.beam.sdk.transforms.Regex;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * A starter example for writing Beam programs.
- *
- * <p>The example takes two strings, converts them to their upper-case
- * representation and logs them.
- *
- * <p>To run this starter example locally using DirectRunner, just
- * execute it without any additional parameters from your favorite development
- * environment.
- *
- * <p>To run this starter example using managed resource in Google Cloud
- * Platform, you should specify the following command-line options:
- *   --project=<YOUR_PROJECT_ID>
- *   --stagingLocation=<STAGING_LOCATION_IN_CLOUD_STORAGE>
- *   --runner=DataflowRunner
- */
+
 public class StarterPipeline {
 	
   private static final Logger LOG = LoggerFactory.getLogger(StarterPipeline.class);
@@ -51,8 +20,7 @@ public class StarterPipeline {
   public interface StarterPipelineOptions extends PipelineOptions {
 
 	    /**
-	     * By default, this example reads from a public dataset containing the text of
-	     * King Lear. Set this option to choose a different input file or glob.
+	     * Set this required option to specify where to read the input.
 	     */
 	    @Description("Path of the file to read from")
 	    @Default.String("gs://dataflow-cloud-academy-281113/beam_info.txt")
